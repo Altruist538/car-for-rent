5e90677cf7ec03a118e69c9deac9de79b1433acf
Altruist538 <khara538@gmail.com>
2023-12-10 11:24:31 +0200

    Update header.js

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,17 +1,20 @@
 import { LinkHome, HeaderBlock, LinkPhone } from './Header.styled';
+import { Outlet } from 'react-router-dom';
 export const Header = () => {
-   const number = "+380730000000"
-    return (
+  const number = '+380730000000';
+  return (
+    <div>
       <HeaderBlock>
-      <nav>
-      <LinkHome to="/">Home</LinkHome>
-      <LinkHome to="/catalog">Cotalog</LinkHome>
-      <LinkHome to="/favorites">Favorites</LinkHome>
-      </nav>
-      <div>
-      <LinkPhone href={`tel: ${number}`}>
-        {number}
-      </LinkPhone>
+        <nav>
+          <LinkHome to="/">Home</LinkHome>
+          <LinkHome to="/catalog">Cotalog</LinkHome>
+          <LinkHome to="/favorites">Favorites</LinkHome>
+        </nav>
+        <div>
+          <LinkPhone href={`tel: ${number}`}>{number}</LinkPhone>
+        </div>
+        <Outlet />
+      </HeaderBlock>
     </div>
-    </HeaderBlock>
-    )}
+  );
+};
